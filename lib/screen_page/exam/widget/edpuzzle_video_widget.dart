fefc4695757c2a5ed19ebe154dd16850e2321190
farhadfoysal <mff585855075@gmail.com>
2025-07-27 22:47:56 +0600

    farhadfoysal 27072503

--- lib/screen_page/exam/widget/edpuzzle_video_widget.dart
+++ lib/screen_page/exam/widget/edpuzzle_video_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+import 'package:pod_player/pod_player.dart';
 
 class EdpuzzleVideoWidget extends StatefulWidget {
   final String videoUrl;
@@ -11,9 +11,8 @@ class EdpuzzleVideoWidget extends StatefulWidget {
 }
 
 class _EdpuzzleVideoWidgetState extends State<EdpuzzleVideoWidget> {
-  late VideoPlayerController _controller;
+  late final PodPlayerController _controller;
   bool _isInitialized = false;
-  bool _isPlaying = false;
   bool _hasError = false;
   String? _errorMessage;
 
@@ -25,38 +24,34 @@ class _EdpuzzleVideoWidgetState extends State<EdpuzzleVideoWidget> {
 
   Future<void> _initializePlayer() async {
     try {
-      _controller = VideoPlayerController.network(
-        widget.videoUrl,
-        // Explicit format hint for better format detection
-        formatHint: VideoFormat.dash,
-      )
-        ..addListener(_updateState)
-        ..setLooping(false)
-        ..initialize().then((_) {
+      _controller = PodPlayerController(
+        playVideoFrom: PlayVideoFrom.network(widget.videoUrl),
+        podPlayerConfig: const PodPlayerConfig(
+          autoPlay: false,
+          isLooping: false,
+          videoQualityPriority: [720, 480, 360],
+        ),
+      )..initialise();
+
+      _controller.addListener(() {
+        if (_controller.isVideoPlaying && !_isInitialized) {
           if (mounted) {
             setState(() => _isInitialized = true);
           }
-        }).catchError((error) {
-          if (mounted) {
-            setState(() {
-              _hasError = true;
-              _errorMessage = 'Unsupported video format';
-            });
-          }
-        });
+        }
+      });
+
+      await _controller.initialise();
+      if (mounted) {
+        setState(() => _isInitialized = true);
+      }
     } catch (e) {
       if (mounted) {
         setState(() {
           _hasError = true;
-          _errorMessage = 'Failed to initialize video player';
+          _errorMessage = 'Failed to initialize video player: ${e.toString()}';
         });
       }
-    }
-  }
-
-  void _updateState() {
-    if (mounted) {
-      setState(() => _isPlaying = _controller.value.isPlaying);
     }
   }
 
@@ -94,13 +89,15 @@ class _EdpuzzleVideoWidgetState extends State<EdpuzzleVideoWidget> {
       return _buildLoadingState();
     }
 
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        VideoPlayer(_controller),
-        _buildPlayPauseOverlay(),
-        _buildProgressBar(),
-      ],
+    return PodVideoPlayer(
+      controller: _controller,
+      podPlayerLabels: const PodPlayerLabels(
+        play: 'Play',
+        pause: 'Pause',
+        error: 'Error',
+        playbackSpeed: 'Speed',
+        settings: 'Settings',
+      ),
     );
   }
 
@@ -115,6 +112,7 @@ class _EdpuzzleVideoWidgetState extends State<EdpuzzleVideoWidget> {
           Text(
             _errorMessage ?? 'Video playback error',
             style: const TextStyle(color: Colors.red),
+            textAlign: TextAlign.center,
           ),
           const SizedBox(height: 16),
           ElevatedButton(
@@ -138,45 +136,6 @@ class _EdpuzzleVideoWidgetState extends State<EdpuzzleVideoWidget> {
     );
   }
 
-  Widget _buildPlayPauseOverlay() {
-    return Positioned.fill(
-      child: GestureDetector(
-        onTap: _togglePlay,
-        child: AnimatedOpacity(
-          opacity: _isPlaying ? 0 : 1,
-          duration: const Duration(milliseconds: 300),
-          child: Container(
-            color: Colors.black26,
-            child: Center(
-              child: Icon(
-                _isPlaying ? Icons.pause : Icons.play_arrow,
-                size: 48,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildProgressBar() {
-    return Positioned(
-      bottom: 0,
-      left: 0,
-      right: 0,
-      child: VideoProgressIndicator(
-        _controller,
-        allowScrubbing: true,
-        colors: VideoProgressColors(
-          playedColor: Theme.of(context).primaryColor,
-          bufferedColor: Colors.grey[300]!,
-          backgroundColor: Colors.grey[600]!,
-        ),
-      ),
-    );
-  }
-
   Future<void> _retryInitialization() async {
     setState(() {
       _hasError = false;
@@ -186,16 +145,213 @@ class _EdpuzzleVideoWidgetState extends State<EdpuzzleVideoWidget> {
     await _initializePlayer();
   }
 
-  void _togglePlay() {
-    setState(() {
-      _isPlaying ? _controller.pause() : _controller.play();
-    });
-  }
-
   @override
   void dispose() {
-    _controller.removeListener(_updateState);
     _controller.dispose();
     super.dispose();
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
+//
+// class EdpuzzleVideoWidget extends StatefulWidget {
+//   final String videoUrl;
+//
+//   const EdpuzzleVideoWidget({Key? key, required this.videoUrl}) : super(key: key);
+//
+//   @override
+//   _EdpuzzleVideoWidgetState createState() => _EdpuzzleVideoWidgetState();
+// }
+//
+// class _EdpuzzleVideoWidgetState extends State<EdpuzzleVideoWidget> {
+//   late VideoPlayerController _controller;
+//   bool _isInitialized = false;
+//   bool _isPlaying = false;
+//   bool _hasError = false;
+//   String? _errorMessage;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _initializePlayer();
+//   }
+//
+//   Future<void> _initializePlayer() async {
+//     try {
+//       _controller = VideoPlayerController.network(
+//         widget.videoUrl,
+//         // Explicit format hint for better format detection
+//         formatHint: VideoFormat.dash,
+//       )
+//         ..addListener(_updateState)
+//         ..setLooping(false)
+//         ..initialize().then((_) {
+//           if (mounted) {
+//             setState(() => _isInitialized = true);
+//           }
+//         }).catchError((error) {
+//           if (mounted) {
+//             setState(() {
+//               _hasError = true;
+//               _errorMessage = 'Unsupported video format';
+//             });
+//           }
+//         });
+//     } catch (e) {
+//       if (mounted) {
+//         setState(() {
+//           _hasError = true;
+//           _errorMessage = 'Failed to initialize video player';
+//         });
+//       }
+//     }
+//   }
+//
+//   void _updateState() {
+//     if (mounted) {
+//       setState(() => _isPlaying = _controller.value.isPlaying);
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       margin: const EdgeInsets.symmetric(vertical: 16),
+//       decoration: BoxDecoration(
+//         borderRadius: BorderRadius.circular(12),
+//         boxShadow: [
+//           BoxShadow(
+//             color: Colors.black.withOpacity(0.15),
+//             blurRadius: 10,
+//             spreadRadius: 1,
+//             offset: const Offset(0, 4),
+//           ),
+//         ],
+//       ),
+//       child: ClipRRect(
+//         borderRadius: BorderRadius.circular(12),
+//         child: AspectRatio(
+//           aspectRatio: 16/9,
+//           child: _buildPlayerContent(),
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Widget _buildPlayerContent() {
+//     if (_hasError) {
+//       return _buildErrorState();
+//     }
+//
+//     if (!_isInitialized) {
+//       return _buildLoadingState();
+//     }
+//
+//     return Stack(
+//       alignment: Alignment.center,
+//       children: [
+//         VideoPlayer(_controller),
+//         _buildPlayPauseOverlay(),
+//         _buildProgressBar(),
+//       ],
+//     );
+//   }
+//
+//   Widget _buildErrorState() {
+//     return Container(
+//       color: Colors.grey[100],
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           const Icon(Icons.error_outline, size: 48, color: Colors.red),
+//           const SizedBox(height: 16),
+//           Text(
+//             _errorMessage ?? 'Video playback error',
+//             style: const TextStyle(color: Colors.red),
+//           ),
+//           const SizedBox(height: 16),
+//           ElevatedButton(
+//             onPressed: _retryInitialization,
+//             child: const Text('Retry'),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   Widget _buildLoadingState() {
+//     return Container(
+//       color: Colors.grey[100],
+//       child: Center(
+//         child: CircularProgressIndicator(
+//           valueColor: AlwaysStoppedAnimation(
+//               Theme.of(context).primaryColor),
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Widget _buildPlayPauseOverlay() {
+//     return Positioned.fill(
+//       child: GestureDetector(
+//         onTap: _togglePlay,
+//         child: AnimatedOpacity(
+//           opacity: _isPlaying ? 0 : 1,
+//           duration: const Duration(milliseconds: 300),
+//           child: Container(
+//             color: Colors.black26,
+//             child: Center(
+//               child: Icon(
+//                 _isPlaying ? Icons.pause : Icons.play_arrow,
+//                 size: 48,
+//                 color: Colors.white,
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Widget _buildProgressBar() {
+//     return Positioned(
+//       bottom: 0,
+//       left: 0,
+//       right: 0,
+//       child: VideoProgressIndicator(
+//         _controller,
+//         allowScrubbing: true,
+//         colors: VideoProgressColors(
+//           playedColor: Theme.of(context).primaryColor,
+//           bufferedColor: Colors.grey[300]!,
+//           backgroundColor: Colors.grey[600]!,
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Future<void> _retryInitialization() async {
+//     setState(() {
+//       _hasError = false;
+//       _errorMessage = null;
+//       _isInitialized = false;
+//     });
+//     await _initializePlayer();
+//   }
+//
+//   void _togglePlay() {
+//     setState(() {
+//       _isPlaying ? _controller.pause() : _controller.play();
+//     });
+//   }
+//
+//   @override
+//   void dispose() {
+//     _controller.removeListener(_updateState);
+//     _controller.dispose();
+//     super.dispose();
+//   }
+// }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,12 +18,14 @@ import flutter_inappwebview_macos
 import google_sign_in_ios
 import mobile_scanner
 import network_info_plus
+import package_info_plus
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
 import video_player_avfoundation
+import wakelock_plus
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -40,11 +42,13 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
